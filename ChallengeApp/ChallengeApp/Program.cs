50f43382e93cfb82560efda98709979727f5450e
Jazon <jjaxiewicz@gmail.com>
2023-12-17 16:29:28 +0100

    Zadanie z 4 dnia

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,16 +1,16 @@
-﻿String imie = "Ewa";
-String plec = "kobieta";
-var wiek = 30;
+﻿String name = "Ewa";
+String sex = "kobieta";
+var age = 30;
 
-if (plec == "kobieta" && wiek < 30)
+if (sex == "kobieta" && age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
-else if (imie == "Ewa" && wiek == 30)
+else if (name == "Ewa" && age == 30)
 {
     Console.WriteLine("Ewa, lat 30");
 }
-else if (plec == "mężczyzna" && wiek < 18)
+else if (sex == "mężczyzna" && age < 18)
 {
     Console.WriteLine("Niepełnoletni mężczyzna");
 }
