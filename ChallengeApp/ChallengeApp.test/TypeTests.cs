e8e4a53e5b62bd2cbca5e9abd02ef400f8f7b4b7
Jazon <jjaxiewicz@gmail.com>
2024-01-01 22:14:26 +0100

    Zadanie z dnia 8 wartości i referencje

---
+++ ChallengeApp/ChallengeApp.test/TypeTests.cs
@@ -0,0 +1,37 @@
+﻿namespace ChallengeApp.test
+{
+    public class TypeTests
+    {
+        [Test]
+        public void ValueTest()
+        {
+            // arrange
+            int number1 = 1;
+            int number2 = 1;
+            string number3 = "4";
+            string number4 = "4";
+
+            var user1 = new User("Jacek", "555");
+            var user2 = new User("Jacek", "666");
+
+            // act
+
+            // assert
+            // verification of the same values
+            Assert.AreEqual(number1, number2);
+            Assert.AreEqual(number3, number4);
+
+            //verification whether the references are equal
+            Assert.AreEqual(user1.Login, user2.Login);
+
+            // verification whether references are different
+            Assert.AreNotEqual(user1, user2);
+            Assert.AreNotEqual(user1.Password, user2.Password);
+        }
+
+        private User GetUser(string name, string password)
+        {
+            return new User(name, password);
+        }
+    }
+}
