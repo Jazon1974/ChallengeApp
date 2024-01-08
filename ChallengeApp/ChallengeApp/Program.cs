6d1805c2d09119876f63326e76b5515b9f229922
Jazon <jjaxiewicz@gmail.com>
2024-01-08 11:45:42 +0100

    Oporawione zadanie o usunięcie numerków ze zmiennych - poprawka druga

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -11,21 +11,21 @@ Console.WriteLine($"Min WithWhile: {statistics.Min}");
 Console.WriteLine($"Max WithWhile: {statistics.Max}");
 Console.WriteLine();
 
-var statistics1 = employee.GetStatisticsWithForEach();
+statistics = employee.GetStatisticsWithForEach();
 
 Console.WriteLine($"Avarage WithForEach: {statistics.Avarge:N2}");
 Console.WriteLine($"Min WithForEach: {statistics.Min}");
 Console.WriteLine($"Max WithForEach: {statistics.Max}");
 Console.WriteLine();
 
-var statistics3 = employee.GetStatisticsWithDoWhile();
+statistics = employee.GetStatisticsWithDoWhile();
 
 Console.WriteLine($"Avarage WithDoWhile: {statistics.Avarge:N2}");
 Console.WriteLine($"Min WithDoWhile: {statistics.Min}");
 Console.WriteLine($"Max WithDoWhile: {statistics.Max}");
 Console.WriteLine();
 
-var statistics2 = employee.GetStatisticsWithFor();
+statistics = employee.GetStatisticsWithFor();
 
 Console.WriteLine($"Avarage WithFor: {statistics.Avarge:N2}");
 Console.WriteLine($"Min WithFor: {statistics.Min}");
